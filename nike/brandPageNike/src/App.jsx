5d8ca740ce1d64476835649a9c0f8567740c88d2
ritwik-raj30 <btech10199.22@bitmesra.ac.in>
2024-05-16 20:50:36 +0530

    project1

---
+++ nike/brandPageNike/src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css";
+import {Navigation} from "../src/component/Navigation"
+import Hero from "../src/component/hero";
+const App=()=>
+{
+  return (
+   <div><Navigation/>
+   <Hero/></div>
+   
+  );
+  
+}
+export  default App
